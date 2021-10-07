8f0bf55c75975fab64e56e271cdd2e0b4cf0cb5f
sittora <sittora@gmail.com>
2021-10-06 20:38:35 -0400

    Added table

--- my-app-frontend/src/components/StockList.js
+++ my-app-frontend/src/components/StockList.js
@@ -2,20 +2,38 @@ import React, { useState, useEffect } from "react";
 import Stock from "./Stock";
 
 function StockList({ stockListArray, title, onViewMore }) {
-    
+
+    const stockRows = stockListArray.map((stock, index) => (
+            <tr key={stock.id}>
+                <td>{stock.name}</td>
+                <td>{stock.ticker_tag}</td>
+                <td>{stock.price}</td>
+                <td>{stock.total_stock}</td>
+            </tr>
+        ));
     return (
         <div id="stock-list-container">
-            <h2 id={title}>{title}</h2>
-            <div id="stock-grid">
-            {stockListArray.map(stock => {
-                return(
-                    <Stock key={stock.id} stock={stock}/>
-                )
-            })}
-            </div>
-            <hr></hr>
-            {/* <button>View More</button> */}
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Ticker Tag</th>
+                    <th>Price</th>
+                    <th>Total Stock</th>
+                </tr>
+                {stockRows}
+            </table>
         </div>
+
+        // <tr>
+        // <td>Alfreds Futterkiste</td>
+        // <td>Maria Anders</td>
+        // <td>Germany</td>
+        // </tr>
+        // <tr>
+        // <td>Centro comercial Moctezuma</td>
+        // <td>Francisco Chang</td>
+        // <td>Mexico</td>
+        // </tr>
     );
 }
 
